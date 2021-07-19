3a147c1fd2a139c1e8658c9ac9e53ac0d928bf3b
CRBroughton <crbroughton@posteo.uk>
2021-07-19 19:06:42 +0100

    explicitly set font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': 'Open-Sans, sans-serif',
+    },
     extend: {},
   },
   variants: {
