ef7e7d4e56a8cc2554ec9f588d13c2c1dfebd9f6
CRBroughton <crbroughton@posteo.uk>
2021-07-20 18:08:00 +0100

    removed commented out code

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -15,19 +15,6 @@
     </div>
   </div>
 </div>
-
-
-
-  <!-- <div class="w-full bg-gray-50 p-3 bottom-0 flex flex-col font-bold text-sm">
-    <h2 class="text-gray-800 m-auto">
-      Want to get in contact?
-      <a href="mailto:crbroughton@posteo.uk" class="text-gray-400"
-        >Email me here.</a
-      >
-    </h2>
-
-
-  </div> -->
 </template>
 
 <script>
