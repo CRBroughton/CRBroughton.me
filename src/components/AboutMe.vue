7bfe92ae2e155987797758b3397f735aa5488c27
CRBroughton <crbroughton@posteo.uk>
2021-07-20 13:41:09 +0100

    created AboutMe.vue

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-full flex text-left text-lg font-medium font-inter px-4 pb-12 text-gray-800 md:justify-center">
+    <div class="flex flex-col max-w-2xl">
+<h3 class="text-2xl md:text-3xl font-medium text-secondary mb-8 font-inter">About Me</h3>
+      <h3 class="text-lg font-semibold mb-4">Hi there, I'm Craig Broughton.</h3>
+      <p class="text-base">
+        I'm a software developer working with the latest technologies. Over the past few years, I have been working on several projects using a variety of languages. My latest project is Forager, a Progressive Web Application written with Vue for mapping publicly accessible foods.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
