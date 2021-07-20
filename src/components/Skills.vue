21be0ecbedc61cc3d504fcb31a64b49e0e4e8270
CRBroughton <crbroughton@posteo.uk>
2021-07-20 14:17:32 +0100

    change para to li change Language to name removed redunant skill.end

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,9 +2,9 @@
   <div class="w-full flex text-left text-lg font-medium font-inter px-4 pb-12 text-gray-800 md:justify-center">
     <div class="flex flex-col max-w-2xl w-full">
       <h1 class="text-xl font-semibold mb-4">Skills</h1>
-      <p v-for="skill in skills" :key="skill">
-        <p class="min-w-1/33 mb-2 mr-2 py-1 px-4 rounded text-sm transition duration-250 ease-in-out bg-green-300 hover:bg-green-500" :class="skill.width + ' ' + skill.end">{{skill.Language}}</p>
-      </p>
+      <li v-for="skill in skills" :key="skill">
+        <p class="min-w-1/33 mb-2 mr-2 py-1 px-4 rounded text-sm transition duration-250 ease-in-out bg-green-300 hover:bg-green-500" :class="skill.width">{{skill.name}}</p>
+      </li>
     </div>
   </div>
 </template>
@@ -14,23 +14,23 @@ export default {
   data() {
     return {
       skills: [
-        { Language: "HTML", width: "w-full", },
-        { Language: "GIT", width: "w-full" },
-        { Language: "Linux", width: "w-full", },
-        { Language: "CSS", width: "w-10/12", },
-        { Language: "Tailwind", width: "w-10/12", },
-        { Language: "Bootstrap", width: "w-10/12", },
-        { Language: "Docker", width: "w-9/12" },
-        { Language: "Javascript", width: "w-9/12" },
-        { Language: "Typescript", width: "w-9/12" },
-        { Language: "Vue", width: "w-9/12" },
-        { Language: "PWA", width: "w-9/12" },
-        { Language: "Astro", width: "w-8/12" },
-        { Language: "Ansible", width: "w-7/12" },
-        { Language: "Python", width: "w-6/12" },
-        { Language: "Express", width: "w-5/12" },
-        { Language: "GraphQL", width: "w-2/12" },
-        { Language: "Gridsome", width: "w-2/12" },
+        { name: "HTML", width: "w-full", },
+        { name: "GIT", width: "w-full" },
+        { name: "Linux", width: "w-full", },
+        { name: "CSS", width: "w-10/12", },
+        { name: "Tailwind", width: "w-10/12", },
+        { name: "Bootstrap", width: "w-10/12", },
+        { name: "Docker", width: "w-9/12" },
+        { name: "Javascript", width: "w-9/12" },
+        { name: "Typescript", width: "w-9/12" },
+        { name: "Vue", width: "w-9/12" },
+        { name: "PWA", width: "w-9/12" },
+        { name: "Astro", width: "w-8/12" },
+        { name: "Ansible", width: "w-7/12" },
+        { name: "Python", width: "w-6/12" },
+        { name: "Express", width: "w-5/12" },
+        { name: "GraphQL", width: "w-2/12" },
+        { name: "Gridsome", width: "w-2/12" },
 
       ],
     };
