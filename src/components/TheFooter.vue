a2ade60f3166bd223a16d9505398f82623354a02
CRBroughton <crbroughton@posteo.uk>
2021-07-20 18:03:09 +0100

    improved footer design

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,15 +1,54 @@
 <template>
-  <div class="w-full bg-gray-50 p-3 bottom-0 flex font-bold text-sm">
-      <h2 class="text-gray-800 m-auto">
-          Want to get in contact? <a href="mailto:crbroughton@posteo.uk" class="text-gray-400">Email me here.</a>
-      </h2>
+
+  <div class="w-full flex text-left text-lg font-medium font-inter px-4 py-12 bg-gray-100 text-gray-800 md:justify-center">
+    <div class="flex w-full flex-col max-w-2xl">
+      <h1 class="text-xl m-auto font-semibold mb-4">Want to get in contact?</h1>
+      <a href="mailto:crbroughton@posteo.uk" class="text-gray-600 m-auto transition duration-250 ease-in-out hover:text-gray-400"
+        >Email me here.</a
+      >
+    <div class="flex m-auto my-4">
+      <a href=""><svg class="mr-4 fill-current text-gray-300 transition duration-250 ease-in-out hover:text-gray-200" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"/></svg></a>
+      <a href="https://github.com/CRBroughton"><svg class="fill-current text-gray-300 transition duration-250 ease-in-out hover:text-gray-200" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
     </div>
+    <div class="m-auto mt-1 text-gray-500 text-sm">
+      <h3>Copyright &copy {{ getCurrentYear().year }} - {{ getCurrentYear().CurrentYear }} All Rights Reserved.</h3>
+    </div>
+  </div>
+</div>
+
+
+
+  <!-- <div class="w-full bg-gray-50 p-3 bottom-0 flex flex-col font-bold text-sm">
+    <h2 class="text-gray-800 m-auto">
+      Want to get in contact?
+      <a href="mailto:crbroughton@posteo.uk" class="text-gray-400"
+        >Email me here.</a
+      >
+    </h2>
+
+
+  </div> -->
 </template>
 
-<script setup>
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getCurrentYear() {
+      const CurrentYear = new Date().getFullYear();
+      const year = 2021
 
+      if (CurrentYear === year) {
+        return { CurrentYear };
+      }
+      return { year, CurrentYear }
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
