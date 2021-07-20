af79e1a5e07f14c7f15fd34246fe8c9bcda5f795
CRBroughton <crbroughton@posteo.uk>
2021-07-20 14:02:17 +0100

    changed header background to white added vertical padding to header at md and above added border to header added negative padding when md and above changed p to a hrefs

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="w-full bg-gray-800 p-3 font-bold text-sm fixed">
-    <ul class="flex justify-center gap-12 md:gap-0">
-      <li class="mr-auto ml-12 hidden md:inline text-white">
-        <p>CRBROUGHTON.ME</p>
+  <div class="w-full bg-white py-8 text-sm md:py-12 md:px-8 border-2 border-gray-100">
+    <ul class="flex justify-center gap-12 md:gap-0 text-gray-800">
+      <li class="mr-auto ml-12 hidden md:inline">
+        <a href>CRBROUGHTON.ME</a>
       </li>
-      <li class="md:pr-12 text-white">
-          PORTFOLIO
+      <li class="md:pr-12">
+          <a href="#aboutme">ABOUT ME</a>
       </li>
-      <li class="md:pr-12 text-white">
-          CONTACT
+      <li class="md:pr-12">
+          <a href="#projects">PROJECTS</a>
+      </li>
+      <li class="md:pr-12">
+          <a href="#skills">SKILLS</a>
+      </li>
+      <li class="md:pr-12">
+          <a href="#footer">CONTACT</a>
       </li>
     </ul>
   </div>
