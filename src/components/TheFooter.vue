7f020f546178e4f4e1ff89fd9cd6f1865962aefc
CRBroughton <crbroughton@posteo.uk>
2021-07-20 14:02:44 +0100

    changed colours for footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full bg-gray-800 p-3 bottom-0 flex font-bold text-sm">
-      <h2 class="text-white m-auto">
-          Want to get in contact? <a href="mailto:crbroughton@posteo.uk" class="text-yellow-400">Email me here.</a>
+  <div class="w-full bg-gray-50 p-3 bottom-0 flex font-bold text-sm">
+      <h2 class="text-gray-800 m-auto">
+          Want to get in contact? <a href="mailto:crbroughton@posteo.uk" class="text-gray-400">Email me here.</a>
       </h2>
     </div>
 </template>
