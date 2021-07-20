97b33e3789182a76382f0c18f8c55b4bbfe816c9
CRBroughton <crbroughton@posteo.uk>
2021-07-20 13:41:20 +0100

    created Projects.vue

---
+++ src/components/Projects.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full flex text-left text-lg font-medium font-inter px-4 pb-12 text-gray-800 md:justify-center">
+    <div class="flex flex-col max-w-2xl">
+      <h1 class="text-xl font-semibold mb-4">Projects</h1>
+      <p class="text-base">All of my projects can be found simply by pressing the button below, which will take you to my Github page.</p>
+      <a href="https://github.com/CRBroughton" class="flex w-44 mt-4 py-2 pl-2 rounded bg-gray-300 justify-center items-center transition duration-250 ease-in-out  hover:bg-gray-200">Github Projects
+
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
