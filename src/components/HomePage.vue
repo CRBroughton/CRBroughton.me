f9ee5ff753ba5e0739b3328591337ed07d66e959
CRBroughton <crbroughton@posteo.uk>
2024-01-24 21:13:23 +0000

    feat: :sparkles: add vue

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="grid justify-center items-center bg-slate-800 w-screen h-screen">
+        <div class="w-screen">
+            <div class="grid grid-cols-2 grid-rows-2 w-full h-screen bg-gray-900">
+                <div class="flex flex-col gap-4 border border-t-0 border-l-0 p-10">
+                    <h1 class="scroll-m-20 font-extrabold tracking-tight text-3xl text-white">Hi there, I'm Craig,</h1>
+                    <h2 class="scroll-m-20 font-extrabold tracking-tight text-xl text-white">I'm a Software Engineer,
+                        focused on delivering <span class="underline underline-offset-4">performant</span>, <span
+                            class="underline underline-offset-4">high-quality</span> applications.</h2>
+                </div>
+                <div class="border border-t-0 border-r-0 border-l-0 flex flex-col justify-center items-center gap-4 p-10">
+                    <h1 class="scroll-m-20 text-6xl font-extrabold tracking-tight text-white uppercase">Projects</h1>
+                </div>
+                <div class="border border-t-0 border-l-0 border-b-0 flex flex-col justify-center items-center gap-4 p-10">
+                    <h1 class="scroll-m-20 text-6xl font-extrabold tracking-tight text-white uppercase">Interests</h1>
+                </div>
+                <div
+                    class="border border-t-0 border-b-0 border-r-0 border-l-0 flex flex-col justify-center items-center gap-4 p-10">
+                    <h1 class="scroll-m-20 text-6xl font-extrabold tracking-tight text-white uppercase">Contact</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
