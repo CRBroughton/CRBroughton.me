cb35f473040e608d288efc429550349249d10fb9
CRBroughton <crbroughton@posteo.uk>
2024-01-25 19:54:57 +0000

    fix sizing for xl

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,7 +7,7 @@ import BottomRight from './BottomRight.vue'
 </script>
 
 <template>
-    <div class="grid justify-center items-center bg-slate-800 max-w-[1920px] max-h-[1080px]">
+    <div class="grid justify-center items-center bg-gray-900 h-screen w-screen xl:max-w-[1920px] xl:max-h-[1080px]">
         <div >
             <div class="grid grid-cols-1 grid-rows-1  xl:grid-cols-2 lg:grid-rows-2 bg-gray-900">
                 <TopLeft />
