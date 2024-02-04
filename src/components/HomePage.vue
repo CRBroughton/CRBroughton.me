b7d050c84b763154209450e1e0ae36f85652f39f
CRBroughton <crbroughton@posteo.uk>
2024-02-04 21:21:00 +0000

    fix: :bug: fix min height on narrow screen

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,8 +7,8 @@ import BottomRight from './BottomRight.vue'
 </script>
 
 <template>
-    <div class="grid justify-center items-center bg-gray-900 h-screen w-screen xl:max-w-[1920px] xl:max-h-[1080px]">
-        <div >
+    <div class="grid justify-center items-center bg-gray-900 min-h-screen w-screen xl:max-w-[1920px] xl:max-h-[1080px]">
+        <div>
             <div class="grid grid-cols-1 grid-rows-1  xl:grid-cols-2 lg:grid-rows-2">
                 <TopLeft />
                 <TopRight />
