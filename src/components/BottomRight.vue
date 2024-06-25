d0fbb904a55262bcafb6292d0966f11b400b5c07
CRBroughton <crbroughton@posteo.uk>
2024-06-25 19:58:19 +0100

    refactor: :recycle: refactor segments to use container component with slots

--- src/components/BottomRight.vue
+++ src/components/BottomRight.vue
@@ -3,31 +3,13 @@
 </script>
 
 <template>
-    <div
-        class="customContainer overflow-scroll border border-t-0 border-b-0 border-r-0 border-l-0 flex flex-col gap-4 p-10 bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors">
-        <h1 class="scroll-m-20 font-extrabold tracking-tight text-3xl ">Wanna chat?</h1>
-        <h2 class="scroll-m-20 font-normal tracking-tight text-xl ">You can find me using any of the following:
-        </h2>
-        <h2 class="scroll-m-20 font-normal tracking-tight text ">Email: <a class="underline underline-offset-4"
-                href="mailto:crbroughton@posteo.uk">CRBroughton@posteo.uk</a></h2>
-        <h2 class="scroll-m-20 font-normal tracking-tight text ">Linkedin: <a class="underline underline-offset-4"
-                href="https://www.linkedin.com/in/craig-broughton-5a2809189">https://www.linkedin.com/in/craig-broughton-5a2809189</a>
-        </h2>
-        <p class="mt-auto ml-auto scroll-m-20 font-normal tracking-tight text-base ">Made with Astro, Vue & Tailwind</p>
-    </div>
+    <h1 class="scroll-m-20 font-extrabold tracking-tight text-3xl ">Wanna chat?</h1>
+    <h2 class="scroll-m-20 font-normal tracking-tight text-xl ">You can find me using any of the following:
+    </h2>
+    <h2 class="scroll-m-20 font-normal tracking-tight text ">Email: <a class="underline underline-offset-4"
+            href="mailto:crbroughton@posteo.uk">CRBroughton@posteo.uk</a></h2>
+    <h2 class="scroll-m-20 font-normal tracking-tight text ">Linkedin: <a class="underline underline-offset-4"
+            href="https://www.linkedin.com/in/craig-broughton-5a2809189">https://www.linkedin.com/in/craig-broughton-5a2809189</a>
+    </h2>
+    <p class="mt-auto ml-auto scroll-m-20 font-normal tracking-tight text-base ">Made with Astro, Vue & Tailwind</p>
 </template>
-
-<style scoped>
-.customContainer {
-    overflow-y: scroll;
-    scrollbar-width: none;
-    /* Firefox */
-    -ms-overflow-style: none;
-    /* Internet Explorer 10+ */
-}
-
-.customContainer::-webkit-scrollbar {
-    /* WebKit */
-    width: 0;
-    height: 0;
-}</style>
---
+++ src/components/GridContainer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="grid grid-cols-1 grid-rows-1 xl:grid-cols-2 lg:grid-rows-2">
+        <div
+            class="customContainer overflow-scroll relative flex flex-col gap-4 border border-t-0 border-r-0 border-l-0 xl:border xl:border-t-0 xl:border-l-0 xl:border-r-1 xl:border-b-10 p-10 w-full h-full bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors">
+            <slot name="top-left" />
+        </div>
+        <div
+            class="customContainer overflow-scroll border border-t-0 border-r-0 border-l-0 flex flex-col gap-4 p-10 bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors">
+            <slot name="top-right" />
+        </div>
+        <div
+            class="customContainer overflow-scroll border border-t-0 border-l-0 border-r-0 xl:border xl:border-t-0 xl:border-l-0 xl:border-b-0 flex flex-col gap-4 p-10 bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors"
+        >
+            <slot name="bottom-left" />
+        </div>
+        <div
+            class="customContainer overflow-scroll border border-t-0 border-b-0 border-r-0 border-l-0 flex flex-col gap-4 p-10 bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors"
+        >
+            <slot name="bottom-right" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.customContainer {
+    overflow-y: scroll;
+    scrollbar-width: none;
+    /* Firefox */
+    -ms-overflow-style: none;
+    /* Internet Explorer 10+ */
+}
+
+.customContainer::-webkit-scrollbar {
+    /* WebKit */
+    width: 0;
+    height: 0;
+}
+</style>
