e3c1216051cab0d09b015cccbed9f0d97dd9bada
CRBroughton <crbroughton@posteo.uk>
2024-06-29 14:23:30 +0100

    feat: :construction: imporove first intro slide

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'spin-slow': 'spin 2s linear infinite',
+			  }
+		},
 	},
 	plugins: [],
 }
