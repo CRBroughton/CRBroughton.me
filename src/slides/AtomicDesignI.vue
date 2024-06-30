6a97db642b92d12b587812e9cdd8ca2c57cea1aa
CRBroughton <crbroughton@posteo.uk>
2024-06-30 09:30:15 +0100

    feat: :construction: add tailwind no scrollbar classes, update font slide

--- src/slides/AtomicDesignI.vue
+++ src/slides/AtomicDesignI.vue
@@ -3,8 +3,6 @@ import { ref } from "vue";
 import Heading from "../components/Heading.vue";
 import SlideArena from '../components/SlideArena.vue'
 import anime from "animejs";
-import Button from "../components/Button.vue";
-import Paragraph from '../components/Paragraph.vue'
 import Atom from '../components/Atom.vue'
 
 const slides = ref<anime.AnimeAnimParams[]>([])
@@ -67,6 +65,20 @@ slides.value = [
         easing: 'easeInOutCubic',
         autoplay: false,
     },
+    {
+        targets: '.type-atoms-foxes',
+        width: 0,
+        duration: 600,
+        easing: 'easeInOutCubic',
+        autoplay: false,
+    },
+    {
+        targets: '.type-atoms-foxes-tokens',
+        width: 400,
+        duration: 600,
+        easing: 'easeInOutCubic',
+        autoplay: false,
+    },
 
 ] satisfies anime.AnimeAnimParams[]
 </script>
@@ -106,10 +118,16 @@ slides.value = [
              
                 <div
                     class="type-atoms absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 flex flex-col gap-8 justify-center items-center">
-                    <div class="flex flex-col gap-4">
+                        <div class="type-atoms-foxes absolute flex flex-col gap-4 overflow-y-scroll whitespace-nowrap no-scrollbar">
                         <Heading size="h1">The quick brown fox jumps over the lazy dog</Heading>
                         <Heading size="h2">The quick brown fox jumps over the lazy dog</Heading>
                         <Heading size="h3">The quick brown fox jumps over the lazy dog</Heading>
+                    </div>
+
+                    <div class="type-atoms-foxes-tokens absolute flex flex-col gap-4 overflow-y-scroll whitespace-nowrap no-scrollbar w-0">
+                        <Heading size="h1">$heading_1</Heading>
+                        <Heading size="h2">$heading_2</Heading>
+                        <Heading size="h3">$heading_3</Heading>
                     </div>
                     <h1
                     class="type-bg-heading absolute font-extrabold uppercase text-opacity-10 text-[500px] text-slate-700 opacity-0 outline-2 drop-shadow-[3.1px_1px_rgba(0,0,0,0.3)]">
