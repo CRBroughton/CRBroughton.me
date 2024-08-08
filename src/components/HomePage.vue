c02d532139e7dc5b5eee620e0563d415a07e868a
CRBroughton <crbroughton@posteo.uk>
2024-08-08 22:28:13 +0100

    create talks section

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,23 +8,18 @@ import GridContainer from './GridContainer.vue'
 </script>
 
 <template>
-    <div class="flex justify-center items-center min-h-screen w-screen bg-slate-900">
-        <div
-            class="grid justify-center items-center min-h-screen w-screen xl:max-w-[1920px] xl:max-h-[1080px] bg-salte-9">
-            <GridContainer>
-                <template #top-left>
-                    <TopLeft />
-                </template>
-                <template #top-right>
-                    <TopRight />
-                </template>
-                <template #bottom-left>
-                    <BottomLeft />
-                </template>
-                <template #bottom-right>
-                    <BottomRight />
-                </template>
-            </GridContainer>
-        </div>
-    </div>
+    <GridContainer>
+        <template #top-left>
+            <TopLeft />
+        </template>
+        <template #top-right>
+            <TopRight />
+        </template>
+        <template #bottom-left>
+            <BottomLeft />
+        </template>
+        <template #bottom-right>
+            <BottomRight />
+        </template>
+    </GridContainer>
 </template>
---
+++ src/components/TalksSections.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import GridContainer from "./GridContainer.vue";
+import Heading from './Heading.vue';
+import Paragraph from './Paragraph.vue'
+
+</script>
+
+<template>
+	<GridContainer class="h-full">
+		<template #top-left>
+			<Heading size="h1">A Cosmic Voyage Through Front End Development</Heading>
+			<Paragraph>
+				Take a cosmic journey through lifecycle of a front end component.
+			</Paragraph>
+			<Paragraph>
+				This presentation attempts to demonstrate the process
+				of building a component from the most atomic tokens,
+				composition of smaller components, all the way through to a fully functioning component.
+			</Paragraph>
+		</template>
+	</GridContainer>
+</template>
