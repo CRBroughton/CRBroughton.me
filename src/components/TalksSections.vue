2d3659b6a08352732d266221c22d240bd10b57d2
CRBroughton <crbroughton@posteo.uk>
2024-09-02 20:54:51 +0100

    refactor: :recycle: move talks section to center (temp until more talks)

--- src/components/TalksSections.vue
+++ src/components/TalksSections.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import GridContainer from "./GridContainer.vue";
 import Heading from './Heading.vue';
 import Paragraph from './Paragraph.vue'
-
 </script>
 
 <template>
-	<GridContainer class="h-full">
-		<template #top-left>
+	<a href="/talks/voyage">
+		<div
+			class="customContainer overflow-scroll relative flex flex-col gap-4  p-10 w-full h-full bg-gray-900 hover:bg-slate-200 text-white hover:text-slate-900 transition-colors">
+
 			<Heading size="h1">A Cosmic Voyage Through Front End Development</Heading>
 			<Paragraph>
 				Take a cosmic journey through lifecycle of a front end component.
@@ -17,6 +17,6 @@ import Paragraph from './Paragraph.vue'
 				of building a component from the most atomic tokens,
 				composition of smaller components, all the way through to a fully functioning component.
 			</Paragraph>
-		</template>
-	</GridContainer>
+		</div>
+	</a>
 </template>
