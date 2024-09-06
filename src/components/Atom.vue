c742913bf0f39525240708e2256c6c70460f26e5
CRBroughton <crbroughton@posteo.uk>
2024-09-06 21:33:14 +0100

    pass name prop

--- src/components/Atom.vue
+++ src/components/Atom.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-const props = defineProps<{colour: string, absolute?: boolean}>()
+const props = defineProps<{name?: string, colour: string, absolute?: boolean}>()
 </script>
 
 <template>
     <div class="animate-spin-slow" :class="{'absolute': props.absolute}">
         <div class="flex justify-center items-center w-32 h-32 border-2 border-white rounded-full">
             <div class="absolute w-4 h-4 bg-white rounded-full top-[-6px]"></div>
-            <div class="w-24 h-24 rounded-full" :class="props.colour">
+            <div class="w-24 h-24 rounded-full" :class="[props.name, props.colour]">
             </div>
         </div>
     </div>
